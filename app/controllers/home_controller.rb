a874e46402651a3b402239ac8df873dd148b2830
Brittany L. Walentin <brittany.walentin@gmail.com>
2015-09-09 21:13:07 -0700

    WIP

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,23 @@
+require 'api_helper'
+
 class HomeController < ApplicationController
+
 
   def index
 
   end
 
-  def get_summoner_id
-    response = ApiHelper.new()
+  def get_summoner
+    # name = params[:summoner_name]
+    # @response = HTTParty.get(SUMMONER + name + AFTER + API_KEY)
+    # raise
+    @response = ApiHelper.new.get_summoner_id(params[:summoner_name])
   end
+
+#   def order_params
+#   params.require(:order).permit(:status, :email, :cc_name, :cc_number,
+#     :cc_expiration, :cc_cvv, :billing_zip, :shipped, :address1, :address2,
+#     :city, :state, :mailing_zip, :mailing_name)
+# end
 
 end
--- lib/api_helper.rb
+++ lib/api_helper.rb
@@ -1,7 +1,15 @@
-API_KEY = ENV['KEY']
-SUMMONER = 'https://api/lol/na/v1.4/summoner/by-name/'
-AFTER = '?api_key='
+class ApiHelper
+  
+  KEY = 'ENV['LOL_KEY']'
+  LOL = 'https://na.api.pvp.net/'
+  SUMMONER = 'api/lol/na/v1.4/summoner/by-name/'
+  AFTER = '?api_key='
 
-def get_summoner_id(name)
-  @response = HTTParty.get(SUMMONER + name + AFTER + API_KEY)
+  # https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/agentscreech?api_key=df17d00f-b3f7-4984-8349-799645c521d2
+
+  def get_summoner_id(name)
+    @response = HTTParty.get(LOL + SUMMONER + name + AFTER + KEY)
+    raise
+  end
+
 end

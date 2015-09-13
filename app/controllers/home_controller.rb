bf6ce873a41e8f02017514163e3d0da32b109bbe
Brittany L. Walentin <brittany.walentin@gmail.com>
2015-09-13 15:00:43 -0700

    Adds flash messages for API non-response.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,30 +2,29 @@ require 'api_helper'
 
 class HomeController < ApplicationController
   PROFILE_ICON = 'http://ddragon.leagueoflegends.com/cdn/5.2.1/img/profileicon/'
+
   def index
   end
 
   def get_summoner
-    if params[:summoner_name].nil?
-        flash[:error] = "Please enter your summoner name."
+    if params[:summoner_name] == ""
+      flash[:error] = "We need your summoner name to find your stats.
+      We're not mind readers!"
+      render 'index'
+    else
+      response = ApiHelper.new.get_summoner_id(params[:summoner_name])
+      if response.parsed_response.include?("400") || response.parsed_response.include?("401") || response.parsed_response.include?("429") || response.parsed_response.include?("500") || response.parsed_response.include?("503")
+        flash[:error] = "Something went wrong and Riot Games is not responding.
+        Please try again later."
         render 'index'
+      elsif response.parsed_response.include?("404")
+        flash[:error] = "Summoner information not found. Please try again."
+        render 'index'
+      else
+         @summoner = parse_response(response)
+         render 'summoner'
+      end
     end
-    response = ApiHelper.new.get_summoner_id(params[:summoner_name])
-    # if response['status'] == 200
-      # @summoner = parse_response(response)
-      # render 'summoner'
-    # elsif response['status'] == 404
-    #   flash[:error] = "Summoner information not found. Please try again."
-    #   render 'index'
-    # elsif
-    #   flash[:error] = "Something went wrong and Riot Games is not responding.
-    #    Please try again later."
-    #    render 'index'
-    #  else
-       @summoner = parse_response(response)
-       render 'summoner'
-
-    # end
   end
 
   def summary

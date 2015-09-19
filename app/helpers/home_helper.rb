c071063cdb712327efbf54bc8334eca8a4aa3623
Brittany L. Walentin <brittany.walentin@gmail.com>
2015-09-19 13:17:23 -0700

    WIP: styling for summary page. Adds link to home page in footer, and style to submit button.

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,9 +1,27 @@
 module HomeHelper
-  def win_image(wins)
+  def winning_image(wins)
     if wins > 50
       image_tag('happy', size: '64x46')
     else
       image_tag('sad', size: '64x64')
+    end
+  end
+
+  def did_you_win(win_field)
+    if win_field
+      image_tag('thumbup.jpg', height: '80', width: '100')
+    else
+      image_tag('thumbdown.jpg', height: '90', width: '100')
+    end
+  end
+
+  def gold_display(amount)
+    gold_rows = ''
+    scaled_down = (amount/1000)
+    scaled_down = scaled_down.round
+
+    scaled_down.times do
+      gold_rows += image_tag('coin')
     end
   end
 

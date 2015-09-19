fcfc045a27b04bf1d10a5313a2c1a48cf3eaa0e7
Brittany L. Walentin <brittany.walentin@gmail.com>
2015-09-19 11:20:17 -0700

    Adds win_image view helper method for happy or sad image for win percentage.

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,19 @@
 module HomeHelper
+  def win_image(wins)
+    if wins > 50
+      image_tag('happy', size: '64x46')
+    else
+      image_tag('sad', size: '64x64')
+    end
+  end
+
 end
+
+
+# def render_stars(rating)
+#     output = ''
+#     if (1..5).include?(rating)
+#       rating.times { output += image_tag('star.png') }
+#     end
+#     output.html_safe
+#   end

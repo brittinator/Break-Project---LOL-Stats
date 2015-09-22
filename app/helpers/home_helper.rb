a2822451ca7d62599def12e9c224fea9315493b9
Brittany L. Walentin <brittany.walentin@gmail.com>
2015-09-21 20:12:46 -0700

    Adds view helper methods for displaying gold piles and if you won or lost (thumbs up\/down).

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -8,23 +8,23 @@ module HomeHelper
   end
 
   def did_you_win(win_field)
-    if win_field
+    if win_field == true
       image_tag('thumbup.jpg', height: '80', width: '100')
     else
       image_tag('thumbdown.jpg', height: '90', width: '100')
     end
   end
 
+
   def gold_display(amount)
     gold_rows = ''
     scaled_down = (amount/1000)
     scaled_down = scaled_down.round
 
-    scaled_down.times do
-      gold_rows += image_tag('coin')
-    end
-  end
+    scaled_down.times { gold_rows += image_tag('coin.jpg', size: '42x64') }
 
+    return gold_rows.html_safe
+  end
 end
 
 
